e5eebe74a7550734d98cda9b653c1ebdcf3fdd7e
jjyy4sun <suntian0@gmail.com>
2020-07-01 21:30:34 +0800

    graph

--- Framework/Common/GraphicsManager.cpp
+++ Framework/Common/GraphicsManager.cpp
@@ -1,2 +1,24 @@
 #include "GraphicsManager.hpp"
 
+namespace My {
+class GraphicsManager
+{
+private:
+    static GraphicsManager& get() {
+        static GraphicsManager Instance;
+        return Instance;
+    }
+public:
+    GraphicsManager();
+    ~GraphicsManager();
+};
+
+GraphicsManager::GraphicsManager()
+{
+}
+
+GraphicsManager::~GraphicsManager()
+{
+}
+
+}
